187df405c47c080a2c1716f5cc8107e2983e0c48
rodrigomazzaroo <rodrigomazzaroo@gmail.com>
2025-11-18 19:11:50 -0300

    Update Boot.js

--- juego/scenes/Boot.js
+++ juego/scenes/Boot.js
@@ -1,1 +1,18 @@
+export default class Boot extends Phaser.Scene {
+    constructor() {
+        super("Boot");
+    }
+
+    preload() {
+        this.load.image("fondo", "assets/fondo.jpg");
+        this.load.spritesheet("player", "assets/player.png", {
+            frameWidth: 96,
+            frameHeight: 96
+        });
+    }
+
+    create() {
+        this.scene.start("MainMenu");
+    }
+}
 
