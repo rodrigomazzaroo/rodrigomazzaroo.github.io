b4d132b73d324197923d979a88b13f00bd327fd8
rodrigomazzaroo <rodrigomazzaroo@gmail.com>
2025-11-18 18:44:15 -0300

    Update MainMenu.js

--- mazzaro-ink-wars/docs/scenes/MainMenu.js
+++ mazzaro-ink-wars/docs/scenes/MainMenu.js
@@ -1,1 +1,24 @@
+export default class MainMenu extends Phaser.Scene {
+    constructor() {
+        super("MainMenu");
+    }
+
+    create() {
+        this.add.text(50, 50, "MAZZARO INK WARS", {
+            fontFamily: "Arial",
+            fontSize: 48,
+            color: "#ffffff"
+        });
+
+        this.add.text(50, 120, "Tocá para comenzar", {
+            fontFamily: "Arial",
+            fontSize: 28,
+            color: "#cccccc"
+        });
+
+        this.input.on("pointerdown", () => {
+            this.scene.start("GameScene");
+        });
+    }
+}
 
