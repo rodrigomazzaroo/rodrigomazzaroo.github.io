a2065894b9b446d7bca44265778d860590a6cb88
rodrigomazzaroo <rodrigomazzaroo@gmail.com>
2025-11-18 19:12:28 -0300

    Update GameScene.js

--- juego/scenes/GameScene.js
+++ juego/scenes/GameScene.js
@@ -1,1 +1,49 @@
+export default class GameScene extends Phaser.Scene {
+    constructor() {
+        super("GameScene");
+    }
+
+    create() {
+        // Fondo
+        this.add.image(0, 0, "fondo").setOrigin(0, 0);
+
+        // Player
+        this.player = this.physics.add.sprite(300, 300, "player", 0);
+        this.player.setCollideWorldBounds(true);
+
+        // Animación
+        this.anims.create({
+            key: "walk",
+            frames: this.anims.generateFrameNumbers("player", { start: 0, end: 5 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+    }
+
+    update() {
+        const speed = 200;
+
+        this.player.setVelocity(0);
+
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-speed);
+            this.player.flipX = true;
+            this.player.play("walk", true);
+        } else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(speed);
+            this.player.flipX = false;
+            this.player.play("walk", true);
+        } else if (this.cursors.up.isDown) {
+            this.player.setVelocityY(-speed);
+            this.player.play("walk", true);
+        } else if (this.cursors.down.isDown) {
+            this.player.setVelocityY(speed);
+            this.player.play("walk", true);
+        } else {
+            this.player.stop();
+        }
+    }
+}
 
